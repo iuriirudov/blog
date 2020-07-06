c50808ba3a0b8ca470d779420c5e2d903241e2fd
Iurii <irudov.nc@gmail.com>
2020-07-06 18:31:12 -0400

    changed environmental variable for DB

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(process.env.DATABASE_URL + 'blog', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
